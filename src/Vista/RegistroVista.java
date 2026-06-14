b475c108c1129ec8bebf9420e2072e2118d955bd
Andre <katherineandrearisco@gmail.com>
2026-06-13 20:15:35 -0500

    Correccion

--- src/Vista/RegistroVista.java
+++ src/Vista/RegistroVista.java
@@ -215,7 +215,7 @@ public class RegistroVista extends javax.swing.JFrame {
     }//GEN-LAST:event_txt_NombreActionPerformed
 
     private void txt_ConfirmarContraseñaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txt_ConfirmarContraseñaActionPerformed
-        // TODO add your handling code here:
+        
     }//GEN-LAST:event_txt_ConfirmarContraseñaActionPerformed
 
     private void btn_RegistrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_RegistrarActionPerformed
